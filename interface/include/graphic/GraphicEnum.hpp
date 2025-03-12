f54906224da785f64c7e3cacb70bdd33539a087c
JsuisSayker <killian.trouve@epitech.eu>
2025-03-12 18:35:43 +0000

    [WIP] File Explorer
    adding a file explorer in the side bar of the project editor

--- interface/include/graphic/GraphicEnum.hpp
+++ interface/include/graphic/GraphicEnum.hpp
@@ -93,12 +93,22 @@ namespace graphic
     struct SpriteEditorData
     {
         bool showColorSelector;
-        Color selectedColor;    
+        Color selectedColor;
         Color defaultColor;
         bool showPixelSidebar;
     };
 
-    struct TabSelectorData {
+    struct ProjectEditorData
+    {
+        bool showFileExplorer;
+        bool showLayer;
+        bool showGameAssets;
+        bool showPixelLinker;
+        bool showScenes;
+    };
+
+    struct TabSelectorData
+    {
         int tabIndex = 0;
     };
 }
