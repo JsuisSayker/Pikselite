4768d9e71b31d781074f6b918dbde49784540ded
JsuisSayker <killian.trouve@epitech.eu>
2025-10-06 00:50:51 +0900

    [UPD] setup.bat file
    Add a verification on the installation of the packages to avoid reinstalling them
    
    Need to test it on real condition like another pc

--- setup.bat
+++ setup.bat
@@ -9,13 +9,10 @@ title PikseliteEngine Setup
 set "PROJECT_NAME=PikseliteEngine"
 set "BUILD_DIR=build"
 set "TARGET_TRIPLET=x64-windows"
-set "CMAKE_DIR=%USERPROFILE%\cmake"
-
-:: -------------------------------------------------
-:: DEFAULT VCPKG PATHS TO CHECK
-:: -------------------------------------------------
+set "VCPKG_HASH_FILE=%USERPROFILE%\.vcpkg_hash"
 set "VCPKG_PATHS=%USERPROFILE%\vcpkg C:\vcpkg"
 set "VCPKG_ROOT="
+set "CMAKE_EXE=C:\Users\killi\cmake\cmake-3.30.0-windows-x86_64\bin\cmake.exe"
 
 :: -------------------------------------------------
 :: SEARCH FOR EXISTING VCPKG
@@ -33,9 +30,8 @@ for %%p in (%VCPKG_PATHS%) do (
 echo === VCPKG Setup ===
 if "!VCPKG_ROOT!" equ "" (
     echo vcpkg not found on this system.
-    if not defined VCPKG_ROOT set "VCPKG_ROOT=%USERPROFILE%\vcpkg"
+    set "VCPKG_ROOT=%USERPROFILE%\vcpkg"
     echo Installing vcpkg to "!VCPKG_ROOT!"...
-
     git clone https://github.com/microsoft/vcpkg "!VCPKG_ROOT!"
     if %ERRORLEVEL% neq 0 (
         echo Failed to clone vcpkg!
@@ -49,88 +45,61 @@ if "!VCPKG_ROOT!" equ "" (
     echo Using existing vcpkg at "!VCPKG_ROOT!"
 )
 
-:: =====================================================
-:: CHECK / INSTALL CMAKE (Portable)
-:: =====================================================
-echo === Checking for CMake ===
-
-set "CMAKE_EXE="
-set "CMAKE_DIR=%USERPROFILE%\cmake"
-
-:: Try finding cmake from PATH
-for /f "delims=" %%i in ('where cmake 2^>nul') do (
-    set "CMAKE_EXE=%%i"
-    goto :found_cmake
+:: -------------------------------------------------
+:: CHECK CMAKE BINARY
+:: -------------------------------------------------
+if not exist "!CMAKE_EXE!" (
+    echo CMake not found at "!CMAKE_EXE!"!
+    exit /b 1
 )
+echo Found CMake at "!CMAKE_EXE!"
 
-:: Try common installation paths
-if exist "%ProgramFiles%\CMake\bin\cmake.exe" (
-    set "CMAKE_EXE=%ProgramFiles%\CMake\bin\cmake.exe"
-    goto :found_cmake
-)
-if exist "%ProgramFiles(x86)%\CMake\bin\cmake.exe" (
-    set "CMAKE_EXE=%ProgramFiles(x86)%\CMake\bin\cmake.exe"
-    goto :found_cmake
-)
-
-:: Try local portable version
-for /r "!CMAKE_DIR!" %%i in (cmake.exe) do (
-    set "CMAKE_EXE=%%i"
-    goto :found_cmake
-)
-
-:: If still not found, install portable version
-echo CMake not found. Installing portable version...
-if not exist "!CMAKE_DIR!" mkdir "!CMAKE_DIR!"
-pushd "!CMAKE_DIR!"
-curl -L -o cmake.zip https://github.com/Kitware/CMake/releases/download/v3.30.0/cmake-3.30.0-windows-x86_64.zip
-if exist cmake.zip (
-    powershell -Command "Expand-Archive -Path 'cmake.zip' -DestinationPath . -Force"
-    del cmake.zip
-
-    for /d %%d in (*) do (
-        if exist "%%d\bin\cmake.exe" (
-            set "CMAKE_EXE=!CMAKE_DIR!\%%d\bin\cmake.exe"
-        )
+:: -------------------------------------------------
+:: DETERMINE IF DEPENDENCIES NEED INSTALLATION
+:: -------------------------------------------------
+set "INSTALL_DEPS=1"
+if exist "%VCPKG_HASH_FILE%" (
+    certutil -hashfile vcpkg.json SHA256 | find /i " " > "%TEMP%\temp_hash.txt"
+    set /p CURRENT_HASH=<"%TEMP%\temp_hash.txt"
+    set /p LAST_HASH=<"%VCPKG_HASH_FILE%"
+    del "%TEMP%\temp_hash.txt"
+    if "!CURRENT_HASH!"=="!LAST_HASH!" (
+        set "INSTALL_DEPS=0"
+        echo vcpkg.json unchanged, skipping dependency installation.
     )
+)
 
-    if not defined CMAKE_EXE (
-        echo Failed to extract CMake executable!
-        popd
+:: -------------------------------------------------
+:: INSTALL DEPENDENCIES (if needed)
+:: -------------------------------------------------
+if "!INSTALL_DEPS!"=="1" (
+    echo === Installing dependencies via vcpkg ===
+
+    :: Setup VS 2022 x64 environment
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+    :: Force vcpkg to use the correct toolset and triplet
+    set VCPKG_PLATFORM_TOOLSET=v143
+    "!VCPKG_ROOT!\vcpkg.exe" integrate install --triplet !TARGET_TRIPLET!
+    if %ERRORLEVEL% neq 0 (
+        echo Failed to install dependencies.
         exit /b 1
     )
-    echo CMake installed locally at "!CMAKE_EXE!"
-) else (
-    echo Failed to download portable CMake!
-    popd
-    exit /b 1
+
+    echo Dependencies installed successfully.
+
+    :: Save current hash
+    certutil -hashfile vcpkg.json SHA256 | find /i " " > "%VCPKG_HASH_FILE%"
 )
-popd
-goto :found_cmake
 
-:found_cmake
-echo Found CMake at "!CMAKE_EXE!"
-set "PATH=%PATH%;%~dp0;%~dp0\bin;!CMAKE_DIR!;%~dp0;!CMAKE_EXE!"
-
-
-:: -------------------------------------------------
-:: INSTALL DEPENDENCIES
-:: -------------------------------------------------
-echo === Installing dependencies via vcpkg ===
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
-"!VCPKG_ROOT!\vcpkg.exe" integrate install
-if %ERRORLEVEL% neq 0 (
-    echo Failed to install dependencies.
-    exit /b 1
-)
-echo Dependencies installed successfully.
 
 :: -------------------------------------------------
 :: CONFIGURE PROJECT
 :: -------------------------------------------------
 echo === Setting up build directory ===
 echo === Configuring project ===
-cmake -B "!BUILD_DIR!" -S . -DCMAKE_TOOLCHAIN_FILE="!VCPKG_ROOT!\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=!TARGET_TRIPLET!
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
+"!CMAKE_EXE!" -B "!BUILD_DIR!" -S . -DCMAKE_TOOLCHAIN_FILE="!VCPKG_ROOT!\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=!TARGET_TRIPLET!
 if %ERRORLEVEL% neq 0 (
     echo CMake configuration failed!
     exit /b 1
@@ -140,7 +109,7 @@ if %ERRORLEVEL% neq 0 (
 :: BUILD PROJECT
 :: -------------------------------------------------
 echo === Building project ===
-cmake --build "!BUILD_DIR!" --config Release
+"!CMAKE_EXE!" --build "!BUILD_DIR!" --config Release
 if %ERRORLEVEL% neq 0 (
     echo Build failed!
     exit /b 1
