513e5282fd915667e5cdbe7d942bc1eac2035d23
JsuisSayker <killian.trouve@epitech.eu>
2025-10-18 22:32:18 +0900

    [UPD] main.cpp in src folder
    change the main to integrate imgui to the project using the SDL2 and OpenGL window instead of creating a new window for imgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,67 @@
 #define SDL_MAIN_HANDLED
-#include <SDL2/SDL.h>
+#include <GL/glew.h>
+#include <SDL.h>
+#include <SDL_opengl.h>
 #include <graphics/interface/interface.hpp>
 #include <graphics/renderer/renderer.hpp>
+
+#include <imgui.h>
+#include <backends/imgui_impl_sdl2.h>
+#include <backends/imgui_impl_opengl3.h>
+
+
+void showImGuiDemo() {
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplSDL2_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::Begin("Hello, ImGui!");
+    ImGui::Text("This is a simple ImGui window.");
+    ImGui::End();
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
 
 int main() {
     graphics::Interface sdlInterface(1280, 720);
     graphics::Renderer renderer(sdlInterface.getWindow(), sdlInterface.getGLContext());
 
-    std::vector<graphics::Pixel> pixels;
-    pixels.push_back({100, 100, 1, 0, 0, 10});
-    pixels.push_back({200, 200, 0, 1, 0, 20});
-    pixels.push_back({300, 300, 0, 0, 1, 30});
+    // --- ImGui Initialization ---
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+    ImGui_ImplSDL2_InitForOpenGL(sdlInterface.getWindow(), sdlInterface.getGLContext());
+    ImGui_ImplOpenGL3_Init("#version 150");
 
-    while (true) {
+    std::vector<graphics::Pixel> pixels = {
+        {100, 100, 1, 0, 0, 10},
+        {200, 200, 0, 1, 0, 20},
+        {300, 300, 0, 0, 1, 30}
+    };
+
+    bool running = true;
+    while (running) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                return 0;
-            }
+            ImGui_ImplSDL2_ProcessEvent(&event);  // let ImGui handle inputs
+            if (event.type == SDL_QUIT)
+                running = false;
         }
+
         renderer.clear();
         renderer.drawPixels(pixels);
+
+        // Draw ImGui content
+        showImGuiDemo();
+
         renderer.present(sdlInterface.getWindow());
     }
 
+    // --- ImGui Cleanup ---
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
 
     return 0;
 }
