bbc539606868dd1b09c97d476e778464a606abdc
Romain Giraud <romain.giraud@epitech.eu>
2026-02-26 15:49:21 +0900

    [ADD] implement ECS integration for GameObjects and register components

--- interface/include/engine/ecs/components/transformComponent.hpp
+++ interface/include/engine/ecs/components/transformComponent.hpp
@@ -6,5 +6,8 @@ namespace ecs::components
         float x, y;
         float rotation;
         float scaleX, scaleY;
+
+        // Previous frame position, used to compute movement delta
+        float prevX = 0.0f, prevY = 0.0f;
     };
 } // namespace ecs::components
