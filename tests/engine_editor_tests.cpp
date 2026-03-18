71042cdacd074d8f2735e863efa522e125344c6e
JsuisSayker <killian.trouve@epitech.eu>
2026-03-18 17:17:18 +0900

    [UPD] tests architecture
    Move some functions out of the engine_editor_tests.cpp file to file dedicated to it

--- tests/engine_editor_tests.cpp
+++ tests/engine_editor_tests.cpp
@@ -11,39 +11,7 @@
 #include <graphics/renderer/camera.hpp>
 #include <tests/imguiSetupForTest.hpp>
 
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <stb_image_write.h>
-
 using namespace Pixel;
-
-TEST(ChunkTests, SetGetBoundary) {
-    Chunk c;
-    EXPECT_EQ(c.get(0, 0), EMPTY);
-    EXPECT_EQ(c.get(CHUNK_SIZE, 0), EMPTY);
-
-    c.set(5, 5, 123);
-    EXPECT_EQ(c.get(5, 5), 123);
-
-    c.set(-1, 0, 999);
-    EXPECT_EQ(c.get(-1, 0), EMPTY);
-}
-
-TEST(ChunkGridTests, ChunkGridOperations) {
-    ChunkGrid grid;
-    EXPECT_EQ(grid.getPixel(0, 0), EMPTY);
-
-    auto& chunk = grid.getOrCreateChunk(0, 0);
-    chunk.set(0, 0, 42);
-    EXPECT_EQ(grid.getPixel(0, 0), 42);
-
-    EXPECT_TRUE(grid.removePixel(0, 0));
-    EXPECT_EQ(grid.getPixel(0, 0), EMPTY);
-
-    chunk.set(1, 1, 77);
-    EXPECT_TRUE(grid.movePixel(1, 1, 2, 2));
-    EXPECT_EQ(grid.getPixel(2, 2), 77);
-    EXPECT_EQ(grid.getPixel(1, 1), EMPTY);
-}
 
 TEST(PixelAttributesTests, DefaultPropertyBehavior) {
     Pixel::DefaultPixelProperties props;
@@ -91,52 +59,6 @@ TEST(GetDesiredSizeTests, FullReturnsDisplaySize) {
 
     EXPECT_EQ(graphics::GetDesiredSize("full", graphics::BarOrientation::Horizontal), 640);
     EXPECT_EQ(graphics::GetDesiredSize("full", graphics::BarOrientation::Vertical), 480);
-
-    testCommon.ShutdownImGuiForTests(window, glContext);
-}
-
-TEST(RendererTests, BasicDrawPaths) {
-    SDL_Window* window = nullptr;
-    SDL_GLContext glContext = nullptr;
-    imguiTest::ImGuiTestCommon testCommon;
-    testCommon.InitImGuiForTests(window, glContext);
-
-    graphics::Renderer renderer(window, glContext);
-
-    // clear/present
-    renderer.clear();
-    renderer.present(window);
-
-    // draw empty doesn't crash
-    graphics::Camera2D camera;
-    renderer.drawPixelsWCamera({}, camera);
-    renderer.drawPixelsOverlay({}, 10.0f);
-
-    // draw one pixel
-    std::vector<graphics::Pixel> pixels{{{0.0f, 0.0f}, {1.0f, 0.0f, 0.0f}}};
-    renderer.drawPixelsOverlay(pixels, 5.0f);
-    renderer.drawPixelsWCamera(pixels, camera, 5.0f);
-
-    // draw grid
-    renderer.drawGrid(camera, 10.0f, glm::vec3(0.0f, 1.0f, 0.0f));
-
-    // Create a small PNG 1x1 pixel (white) in a temp file.
-    const std::string tmpPath = (std::filesystem::temp_directory_path() / "test_tex.png").string();
-    unsigned char texPixels[3] = {255, 255, 255};
-    stbi_write_png(tmpPath.c_str(), 1, 1, 3, texPixels, 1 * 3);
-
-    GLuint texId = renderer.loadTexture(tmpPath);
-    EXPECT_NE(texId, 0u);
-
-    graphics::Sprite2D sprite;
-    sprite.position = glm::vec2(0.0f);
-    sprite.size = glm::vec2(1.0f);
-    sprite.textureID = texId;
-
-    renderer.drawSprite(sprite, camera);
-    renderer.unloadTexture(texId);
-
-    std::filesystem::remove(tmpPath);
 
     testCommon.ShutdownImGuiForTests(window, glContext);
 }
