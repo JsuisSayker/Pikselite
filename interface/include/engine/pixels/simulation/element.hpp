9a918bb1be27f76734b80fd4f23bd319fde6ae32
arthurgauffre <arth.gauffre@gmail.com>
2026-04-17 16:16:06 +0900

    [UPD] Comment code simulation part

--- interface/include/engine/pixels/simulation/element.hpp
+++ interface/include/engine/pixels/simulation/element.hpp
@@ -30,10 +30,67 @@ struct ElementDefinition {
 
 extern ElementDefinition g_elements[256];
 
+/**
+ * @brief Attempts to move a pixel from one cell to another.
+ * @param grid Simulation grid.
+ * @param x Source global X.
+ * @param y Source global Y.
+ * @param nx Destination global X.
+ * @param ny Destination global Y.
+ * @return `true` if movement or swap occurred, otherwise `false`.
+ */
 bool tryMove(ChunkGrid& grid, int x, int y, int nx, int ny);
+
+/**
+ * @brief Updates one water pixel behavior.
+ * @param grid Simulation grid.
+ * @param x Global X coordinate.
+ * @param y Global Y coordinate.
+ * @return void
+ */
 void updateWater(ChunkGrid& grid, int x, int y);
+
+/**
+ * @brief Updates one sand pixel behavior.
+ * @param grid Simulation grid.
+ * @param x Global X coordinate.
+ * @param y Global Y coordinate.
+ * @return void
+ */
 void updateSand(ChunkGrid& grid, int x, int y);
+
+/**
+ * @brief Updates one fire pixel behavior.
+ * @param grid Simulation grid.
+ * @param x Global X coordinate.
+ * @param y Global Y coordinate.
+ * @return void
+ */
 void updateFire(ChunkGrid& grid, int x, int y);
+
+/**
+ * @brief Updates one stone pixel behavior.
+ * @param grid Simulation grid.
+ * @param x Global X coordinate.
+ * @param y Global Y coordinate.
+ * @return void
+ */
 void updateStone(ChunkGrid& grid, int x, int y);
+
+/**
+ * @brief Updates one dirt pixel behavior.
+ * @param grid Simulation grid.
+ * @param x Global X coordinate.
+ * @param y Global Y coordinate.
+ * @return void
+ */
 void updateDirt(ChunkGrid& grid, int x, int y);
+
+/**
+ * @brief Updates one debug pixel behavior.
+ * @param grid Simulation grid.
+ * @param x Global X coordinate.
+ * @param y Global Y coordinate.
+ * @return void
+ */
 void updateDebug(ChunkGrid& grid, int x, int y);
