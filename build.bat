c4e7d9066a86066ed6daed11917989bca34d511d
JsuisSayker <killian.trouve@epitech.eu>
2026-05-01 20:06:28 +0900

    [UPD] staticChecks.yaml
    Make the setup of MSVC conditional in build.bat file

--- build.bat
+++ build.bat
@@ -65,8 +65,11 @@ if not exist "%VCPKG_OVERLAY_PORTS%" (
 :: SETUP MSVC
 :: -------------------------------------------------
 echo === Setup MSVC ===
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
-if %ERRORLEVEL% neq 0 exit /b 1
+if "%CI%"=="" (
+    echo === Setup MSVC ===
+    call "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
+    if %ERRORLEVEL% neq 0 exit /b 1
+)
 
 :: -------------------------------------------------
 :: SETUP VCPKG (local, reproducible)
